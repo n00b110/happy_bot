7c965e25df5d10a939f0ede0abe7b060b51af242
Immanuel Olaoye <olaoye939@gmail.com>
2023-07-15 16:05:01 -0500

    cleaned up some code

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
-import images
+import re
 from PIL import Image
-import io
 from dotenv import dotenv_values
 
 env_vars = dotenv_values()
@@ -13,20 +12,33 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 
 
+def run_bot():
+    @bot.event
+    async def on_message(msg):
+        if msg.author == bot.user:
+            return
 
-@bot.event
-async def on_message(msg):
-    if msg.author == bot.user:
-        return
+        elif msg.attachments:
+            for attachment in msg.attachments:
+                # img_bytes = await attachement.read()
+                # with open("image.jg", "wb") as f:
+                #     f.write(img_bytes)
+                # image = Image.open("image.jpg").convert("L")
+                # image.save("gray.jpg")
+                # await msg.channel.send(file=discord.File('gray.jpg', filename='gray_image.png'))
+                print(attachment.content_type)
+                if attachment.content_type.startswith('image/'):
+                    filename = re.search(r'[^/]+$', attachment.url).group()
+                    img_bytes = await attachment.read()
+                    new_filename = f"{filename + '_gray.jpeg'}"
+                    with open(new_filename, "wb") as f:
+                        f.write(img_bytes)
+                    image = Image.open(new_filename).convert("L")
+                    image.save(new_filename)
+                    await msg.channel.send(file=discord.File(new_filename, filename='gray_image.png'))
+                
+                
+    bot.run(TOKEN)
 
-    elif msg.attachments:
-        for attachement in msg.attachments:
-            img_bytes = await attachement.read()
-            with open("image.jpg", "wb") as f:
-                f.write(img_bytes)
-            image = Image.open("image.jpg").convert("L")
-            image.save("gray.jpg")
-            await msg.channel.send(file=discord.File('gray.jpg', filename='gray_image.png'))
-            
-            
-bot.run(TOKEN)
+
+run_bot()
